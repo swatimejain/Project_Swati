1b6ee94cf5bc3269f4534394b0735292c1bec913
swatimejain <swati.jain1487@gmail.com>
2015-08-08 10:23:31 +0530

    modified home and geo

--- geo.js
+++ geo.js
@@ -57,3 +57,47 @@ document.getElementById("city1").innerHTML=city.long_name;
 	  
     });
   }
+
+  
+  window.onload = function() {
+
+  // Check for LocalStorage support.
+  if (localStorage) {
+
+    // Add an event listener for form submissions
+    document.getElementById('contactForm').addEventListener('submit', function() {
+      // Get the value of the name field.
+      var name = document.getElementById('name').value;
+
+      // Save the name in localStorage.
+      localStorage.setItem('name', name);
+	  
+	  
+	  
+	  
+  // Retrieve the users name.
+  var name = localStorage.getItem('name');
+
+  if (name != "undefined" || name != "null") {
+    document.getElementById('welcomeMessage').innerHTML = "Hello " + name + "!";
+  } else
+    document.getElementById('welcomeMessage').innerHTML = "Hello!";
+
+
+
+localStorage.removeItem('name');
+localStorage.clear();
+    });
+
+  }
+
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
