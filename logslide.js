b5b09f3adc46111d6a2dd72bb54a9ee8a7f56d53
swatimejain <swati.jain1487@gmail.com>
2015-08-15 21:29:30 +0530

    added login ls

--- logslide.js
+++ logslide.js
@@ -17,3 +17,28 @@ $(document).ready(function() {
 	});		
 		
 });
+
+
+function store(theForm) {
+    document.getElementById('welcomeMessage').innerHTML = "";
+    var inputUsername= theForm["usernamet"];
+    var inputPassword= theForm["passwordt"];
+    localStorage.setItem("usernamet", inputUsername.value);
+    localStorage.setItem("passwordt", inputPassword.value);
+    document.getElementById('welcomeMessage').innerHTML = "Welcome " + localStorage.getItem('usernamet') + "!";
+    return false;
+} 
+
+function login(theForm) {
+    document.getElementById('welcomeMessage').innerHTML = "";
+    var inputUsername = theForm["usernamet"];
+    var inputPassword = theForm["passwordt"];
+    var username = inputUsername.value;
+    var password = inputPassword.value;
+    if ((username == localStorage.getItem('usernamet')) && (password == localStorage.getItem('passwordt'))) {
+     document.getElementById('welcomeMessage').innerHTML = "Welcome " + localStorage.getItem('usernamet') + "!";
+      } else {
+     document.getElementById('welcomeMessage').innerHTML = "Invalid Log-in!";
+    }
+    return false;
+}
